d8d7003e8a40854c0b6d2354846b6315dfc44b5a
Patton6 <pjmullin6@comcast.net>
2017-01-18 09:03:18 -0500

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,9 +1,11 @@
-void setup() {
-  // put your setup code here, to run once:
+int TankTemp1 = 0; //initializes temperature variable for tank 1
+int TankTemp2 = 0; //initializes temperature variable for tank 2
+int TankTemp3 = 0; //initializes temperature variable for tank 3
 
+void setup() {
+  Serial.begin(9600); //sets the bit rate to communicate with the computer
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
