ff8fa060694325ed5a58414d11125327738a60cd
Patton6 <pjmullin6@comcast.net>
2017-01-22 13:23:43 -0500

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,3 +1,4 @@
+/*Initial Variable Declarations*/
 //Tank Temperature Variables
 float TankTemp1 = 0; //initializes temperature variable for tank 1
 float TankTemp2 = 0; //initializes temperature variable for tank 2
@@ -12,42 +13,60 @@ int HeatElPin2 = 11; //sets the pin for the heating element for tank 2
 int HeatElPin3 = 12; //sets the pin for the heating element for tank 3
 
 int setup() {
+  /*Initial Arduino Set Up*/
   Serial.begin(9600); //sets the bit rate to communicate with the computer
+  pinMode(HeatElPin1, OUTPUT); //sets up tank 1 heating element's pin to output
+  pinMode(HeatElPin2, OUTPUT); //sets up tank 2 heating element's pin to output
+  pinMode(HeatElPin3, OUTPUT); //sets up tank 3 heating element's pin to output
+  
   /*Primary Heating Test on Startup*/
-  //Temperature sensor 1 code
+  //Temperature sensor 1 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage1 = analogRead(OutPutPin1);
   float millivolts1 = (rawvoltage1/1024.0) * 5000;
   float fahrenheit1 = millivolts1/10;
   Serial.print(fahrenheit1);
   Serial.println(" degrees Fahrenheit, ");
-  //Temperature sensor 2 code
+  //Temperature sensor 2 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage1 = analogRead(OutPutPin1);
   float millivolts1 = (rawvoltage1/1024.0) * 5000;
   float fahrenheit1 = millivolts1/10;
   Serial.print(fahrenheit1);
   Serial.println(" degrees Fahrenheit, Sesnor 2");
-  //Temperature sesnor 3 code
+  //Temperature sesnor 3 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage3 = analogRead(OutPutPin3);
   float millivolts3 = (rawvoltage3/1024.0) * 5000;
   float fahrenheit3 = millivolts3/10;
   Serial.print(fahrenheit3);
   Serial.println(" degrees Fahrenheit.");
+  
+  /*Logic Gates for Initial Heating*/
+  //Heating for Initial Hatching of the Brine Shrimp
+  if (fahrenheit1 <= 80){
+      digitalWrite(HeatElPin1, HIGH); //sets the pin for tank 1 heating element to 5V 
+  }
+  if (fahrenheit2 <= 80){
+      digitalWrite(HeatElPin1, HIGH); //sets the pin for tank 2 heating element to 5V 
+  }
+  if (fahrenheit3 <= 80){
+      digitalWrite(HeatElPin1, HIGH); //sets the pin for tank 3 heating element to 5V 
+  }
 }
 
 void loop() {
-  //Temperature sensor 1 code
+  /*Continuous Water Temperature Testing*/
+  //Temperature sensor 1 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage1 = analogRead(OutPutPin1);
   float millivolts1 = (rawvoltage1/1024.0) * 5000;
   float fahrenheit1 = millivolts1/10;
   Serial.print(fahrenheit1);
   Serial.println(" degrees Fahrenheit, ");
-  //Temperature sensor 2 code
+  //Temperature sensor 2 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage1 = analogRead(OutPutPin1);
   float millivolts1 = (rawvoltage1/1024.0) * 5000;
   float fahrenheit1 = millivolts1/10;
   Serial.print(fahrenheit1);
   Serial.println(" degrees Fahrenheit, Sesnor 2");
-  //Temperature sesnor 3 code
+  //Temperature sesnor 3 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage3 = analogRead(OutPutPin3);
   float millivolts3 = (rawvoltage3/1024.0) * 5000;
   float fahrenheit3 = millivolts3/10;
