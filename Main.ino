d01dc98bec843e37f0552608831f21e28aeab518
Patton6 <pjmullin6@comcast.net>
2017-01-22 13:11:34 -0500

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -6,9 +6,32 @@ float TankTemp3 = 0; //initializes temperature variable for tank 3
 int OutPutPin1 = 0; //sets the pin for the output for tank 1 sensor
 int OutPutPin2 = 1; //sets the pin for the output for tank 2 sensor
 int OutPutPin3 = 2; //sets the pin for the output for tank 3 sensor
+//Heating Pad Variables
+int HeatElPin1 = 10; //sets the pin for the heating element for tank 1
+int HeatElPin2 = 11; //sets the pin for the heating element for tank 2
+int HeatElPin3 = 12; //sets the pin for the heating element for tank 3
 
-void setup() {
+int setup() {
   Serial.begin(9600); //sets the bit rate to communicate with the computer
+  /*Primary Heating Test on Startup*/
+  //Temperature sensor 1 code
+  int rawvoltage1 = analogRead(OutPutPin1);
+  float millivolts1 = (rawvoltage1/1024.0) * 5000;
+  float fahrenheit1 = millivolts1/10;
+  Serial.print(fahrenheit1);
+  Serial.println(" degrees Fahrenheit, ");
+  //Temperature sensor 2 code
+  int rawvoltage1 = analogRead(OutPutPin1);
+  float millivolts1 = (rawvoltage1/1024.0) * 5000;
+  float fahrenheit1 = millivolts1/10;
+  Serial.print(fahrenheit1);
+  Serial.println(" degrees Fahrenheit, Sesnor 2");
+  //Temperature sesnor 3 code
+  int rawvoltage3 = analogRead(OutPutPin3);
+  float millivolts3 = (rawvoltage3/1024.0) * 5000;
+  float fahrenheit3 = millivolts3/10;
+  Serial.print(fahrenheit3);
+  Serial.println(" degrees Fahrenheit.");
 }
 
 void loop() {
@@ -29,7 +52,7 @@ void loop() {
   float millivolts3 = (rawvoltage3/1024.0) * 5000;
   float fahrenheit3 = millivolts3/10;
   Serial.print(fahrenheit3);
-  Serial.println(" degrees Fahrenheit, ");
+  Serial.println(" degrees Fahrenheit.");
   
   delay(10000);
 }
