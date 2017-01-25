54ee889d3dc50d1f872a451e7c0f4227e1eb54aa
Patton6 <pjmullin6@comcast.net>
2017-01-25 09:32:51 -0500

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -19,11 +19,9 @@ long int ValToWrite; //value to write variable
 long int X; //variable for counting functions
 long int StartLogTime = 0; //varibale to log time since program started
 
-int setup() {
+void setup() {
   /*Initial Arduino Set Up*/
   Serial.begin(9600); //sets the bit rate to communicate with the computer
-  Serial.print("IPA"); //sets the vdip to use ascii numbers for user to read
-  serial.print(13, BYTE); //return character to tell vdip that it is end of message
   pinMode(HeatElPin1, OUTPUT); //sets up tank 1 heating element's pin to output
   pinMode(HeatElPin2, OUTPUT); //sets up tank 2 heating element's pin to output
   pinMode(HeatElPin3, OUTPUT); //sets up tank 3 heating element's pin to output
@@ -34,19 +32,19 @@ int setup() {
   float millivolts1 = (rawvoltage1/1024.0) * 5000;
   float fahrenheit1 = millivolts1/10;
   Serial.print(fahrenheit1);
-  Serial.println(" degrees Fahrenheit, ");
+  Serial.println(" degrees Fahrenheit, Tank 1,");
   //Temperature sensor 2 code (Do not EDIT, comes from Manufacture's Website)
-  int rawvoltage1 = analogRead(OutPutPin1);
-  float millivolts1 = (rawvoltage1/1024.0) * 5000;
-  float fahrenheit1 = millivolts1/10;
-  Serial.print(fahrenheit1);
-  Serial.println(" degrees Fahrenheit, Sesnor 2");
+  int rawvoltage2 = analogRead(OutPutPin2);
+  float millivolts2 = (rawvoltage2/1024.0) * 5000;
+  float fahrenheit2 = millivolts2/10;
+  Serial.print(fahrenheit2);
+  Serial.println(" degrees Fahrenheit, Tank 2,");
   //Temperature sesnor 3 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage3 = analogRead(OutPutPin3);
   float millivolts3 = (rawvoltage3/1024.0) * 5000;
   float fahrenheit3 = millivolts3/10;
   Serial.print(fahrenheit3);
-  Serial.println(" degrees Fahrenheit.");
+  Serial.println(" degrees Fahrenheit, Tank 3.");
   
   /*Logic Gates for Initial Heating*/
   //Heating for Initial Hatching of the Brine Shrimp
@@ -54,45 +52,34 @@ int setup() {
       digitalWrite(HeatElPin1, HIGH); //sets the pin for tank 1 heating element to 5V 
   }
   if (fahrenheit2 <= 68){
-      digitalWrite(HeatElPin2, HIGH); //sets the pin for tank 2 heating element to 5V 
+      digitalWrite(HeatElPin1, HIGH); //sets the pin for tank 2 heating element to 5V 
   }
   if (fahrenheit3 <= 68){
-      digitalWrite(HeatElPin3, HIGH); //sets the pin for tank 3 heating element to 5V 
+      digitalWrite(HeatElPin1, HIGH); //sets the pin for tank 3 heating element to 5V 
   }
 }
-
- if (fahrenheit1 >= 79){
-      digitalWrite(HeatElPin1, LOW); //sets the pin for tank 1 heating element to 5V 
-  }
-  if (fahrenheit2 >= 79){
-      digitalWrite(HeatElPin2, LOW); //sets the pin for tank 2 heating element to 5V 
-  }
-  if (fahrenheit3 >= 79){
-      digitalWrite(HeatElPin3, LOW); //sets the pin for tank 3 heating element to 5V 
-  }
-}
-
 
 void loop() {
   /*Continuous Water Temperature Testing*/
+  Serial.println("Temperatures");
+  
   //Temperature sensor 1 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage1 = analogRead(OutPutPin1);
   float millivolts1 = (rawvoltage1/1024.0) * 5000;
   float fahrenheit1 = millivolts1/10;
   Serial.print(fahrenheit1);
-  Serial.println(" degrees Fahrenheit, ");
+  Serial.println(" degrees Fahrenheit, Tank 1,");
   //Temperature sensor 2 code (Do not EDIT, comes from Manufacture's Website)
-  int rawvoltage1 = analogRead(OutPutPin1);
-  float millivolts1 = (rawvoltage1/1024.0) * 5000;
-  float fahrenheit1 = millivolts1/10;
-  Serial.print(fahrenheit1);
-  Serial.println(" degrees Fahrenheit, Sesnor 2");
+  int rawvoltage2 = analogRead(OutPutPin2);
+  float millivolts2 = (rawvoltage2/1024.0) * 5000;
+  float fahrenheit2 = millivolts2/10;
+  Serial.print(fahrenheit2);
+  Serial.println(" degrees Fahrenheit, Tank 2,");
   //Temperature sesnor 3 code (Do not EDIT, comes from Manufacture's Website)
   int rawvoltage3 = analogRead(OutPutPin3);
   float millivolts3 = (rawvoltage3/1024.0) * 5000;
   float fahrenheit3 = millivolts3/10;
   Serial.print(fahrenheit3);
-  Serial.println(" degrees Fahrenheit.");
+  Serial.println(" degrees Fahrenheit, Tank 3.");
   
   delay(10000);
-}
